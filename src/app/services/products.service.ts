92b070b844103b2c227545b12a419613dd72af06
JacobFeat <jakubk198@gmail.com>
2022-07-07 20:55:11 +0200

    change URLs

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Product } from '../products-list/product';
+
+const PORT = '8080'
+const URL = `http://localhost:${PORT}/api/`;
 
 @Injectable({
   providedIn: 'root',
@@ -9,29 +11,28 @@ export class ProductsService {
   constructor(private http: HttpClient) {}
 
   getAllProducts() {
-    return this.http.get('http://localhost:8080/api/all');
+    return this.http.get(URL + 'products/all');
   }
 
   getAllCategories() {
-    return this.http.get('http://localhost:8080/api/get/categories');
+    return this.http.get(URL + 'categories');
   }
 
   getOnlySelectedCategory(category: string) {
-    return this.http.get('http://localhost:8080/api/get/' + category);
+    return this.http.get(URL + 'category/' + category);
   }
 
   addProduct(req: any) {
     const headers = { 'content-type': 'application/json' };
-    // const body = JSON.stringify(req);
     const body = req;
     console.log(body);
 
-    return this.http.post('http://localhost:8080/api/new', body, {
+    return this.http.post(URL + 'product/', body, {
       headers: headers,
     });
   }
 
   deleteProduct(id: string) {
-    return this.http.delete('http://localhost:8080/api/delete/' + id);
+    return this.http.delete(URL + 'product/' + id);
   }
 }
